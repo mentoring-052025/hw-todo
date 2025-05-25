8ad77514af168c519dc509d283e1c7792942e0d0
uusen-hub <triin.uusen.finances@gmail.com>
2025-05-25 14:13:31 +0300

    dev: npm install react-router-dom; replace conditional stats with route paths Basic implementation, following guide on: https://hygraph.com/blog/routing-in-react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import WrapHTML from './components/wrapper/WrapHTML';
-import WrapUI from './components/wrapper/WrapUI';
-import { useState } from 'react';
-
+import { Routes, Route, useNavigate } from "react-router-dom";
+import WrapHTML from "./components/wrapper/WrapHTML";
+import WrapUI from "./components/wrapper/WrapUI";
+import { useState } from "react";
 
 function App() {
-
   /* states that control which version of the app to show, version are split into "wrappers" */
   /* it toggles the value either html or UI (html = version of the app with HTML UI components, UI = version with material UI components */
-  const [showVer, setShowVer]  = useState("html");
-  const setShowVerVal = () =>  setShowVer(showVer === "html" ? "ui": "html");
+  const [showVer, setShowVer] = useState("html");
+  const routepath = useNavigate();
+  const setShowVerVal = () => {
+    if (showVer === "html") {
+      setShowVer("ui");
+      routepath("testui");
+    } else {
+      setShowVer("html");
+      /* goes back to previous page, in this first draft dev mode there are only two path - home and /testui, so going back will always go to "/" */
+      /* routepath(-1);*/
+      /* navitate to home */
+      routepath("");
+    }
+  };
 
   return (
     <>
-    
-      <div className="AppWrapper"> 
+      <div className="AppWrapper">
         <button onClick={setShowVerVal}>HTML/UI</button>
-      {/* For learning purposes I will practice boht HTML and Material UI design and will try to replicate draft version of both*/}
-      {/* Manipulate the true/false state in React Dev tools to pick which version to show; I will add a promp with useEffect to ask for the user to pick one on first load */}
-      {showVer === "html" && <WrapHTML />}
-      {showVer === "ui" && <WrapUI />}
+        {/* For learning purposes I will practice boht HTML and Material UI design and will try to replicate draft version of both*/}
+        {/* Manipulate the true/false state in React Dev tools to pick which version to show; I will add a promp with useEffect to ask for the user to pick one on first load */}
+        {/* {showVer === "html" && <WrapHTML />} */}
+        {/* {showVer === "ui" && <WrapUI />} */}
+
+        {/* Routes + Route must be always combined of React.Fragments */}
+        {/* When return includes different elements, <Routes> must be wrapped only around the elements that are subject to routing */}
+        <Routes>
+          <Route path="/" element={<WrapHTML />} />
+          <Route path="/testui" element={<WrapUI />} />
+        </Routes>
       </div>
     </>
   );
--- src/components/wrapper/WrapHTML.js
+++ src/components/wrapper/WrapHTML.js
@@ -1,28 +1,29 @@
 /*import './App.css';*/
-import AppBoiler from '../html/AppBoiler';
-import HeaderDiv from '../html/Header';
-import BottomBarDiv from '../html/Bottombar';
-import { useState } from 'react';
-
+import AppBoiler from "../html/AppBoilerHTML";
+import HeaderDiv from "../html/HeaderHTML";
+import BottomBarDiv from "../html/Bottombar";
+import { useState } from "react";
 
 function WrapHTML() {
-
-  const [isShow1, setShow]  = useState(true);
-  const setShowValue1 = () => setShow(prev => prev = !prev);
-  const [isShow2, setShow2]  = useState(true);
-  const setShowValue2 = () => setShow2(prev => prev = !prev);
+  const [isShow1, setShow] = useState(true);
+  const setShowValue1 = () => setShow((prev) => (prev = !prev));
+  const [isShow2, setShow2] = useState(true);
+  const setShowValue2 = () => setShow2((prev) => (prev = !prev));
 
   return (
     <>
-      <div className="MobileHTML"> 
-      <HeaderDiv />
-      <div className='CustomAdmin'>
-        
-        <button className='BtnTmp1' onClick={setShowValue1}>Show/hide content HTML</button>
-        <button className='BtnTmp2' onClick={setShowValue2}>Show/hide bottom nav HTML</button>
-      </div>
-      {isShow1 && <AppBoiler />}
-      {isShow2 && <BottomBarDiv />}
+      <div className="MobileHTML">
+        <HeaderDiv />
+        <div className="CustomAdmin">
+          <button className="BtnTmp1" onClick={setShowValue1}>
+            Show/hide content HTML
+          </button>
+          <button className="BtnTmp2" onClick={setShowValue2}>
+            Show/hide bottom nav HTML
+          </button>
+        </div>
+        {isShow1 && <AppBoiler />}
+        {isShow2 && <BottomBarDiv />}
       </div>
     </>
   );
